2fedcf626b8e97fd00d97b871e0d3fdefebd93d4
dhcsbalutbomber <richard.bach@dhcs.ca.gov>
2021-02-24 17:01:29 -0800

    fixed issue with name of iam codepipeline module

--- spinnaker.tf
+++ spinnaker.tf
@@ -14,7 +14,7 @@ module "kms" {
 }
 
 module "iam_codepipeline" {
-  source     = "./modules/iam-codepipeline"
+  source     = "./modules/iam_codepipeline"
   account_id = data.aws_caller_identity.current.account_id
   full_name  = local.full_name
   aws_s3_bucket_artifacts_arn = module.s3.aws_s3_bucket_artifacts_arn
@@ -25,7 +25,7 @@ module "codepipeline" {
   source     = "./modules/codepipeline"
   aws_s3_bucket_artifacts_bucket = module.s3.aws_s3_bucket_artifacts_bucket
   aws_kms_alias_this_arn = module.kms.aws_kms_alias_this_arn
-  codepipeline_role_arn = module.iam-codepipeline.aws_iam_role_this_arn
+  codepipeline_role_arn = module.iam_codepipeline.aws_iam_role_this_arn
   full_name = local.full_name
   github_configuration = var.github_configuration
 }
