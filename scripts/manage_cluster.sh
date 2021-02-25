446a9db3612acdee7a6992523d1780b7d8e5670f
dhcsbalutbomber <richard.bach@dhcs.ca.gov>
2021-02-25 10:13:49 -0800

    refactor build_closter.buildspec.yml and changed az for cluster in eks

--- scripts/manage_cluster.sh
+++ scripts/manage_cluster.sh
@@ -5,5 +5,5 @@ then
     echo "$1 exists, nothing to do"
 else
     echo "$1 does not exist, creating"
-    eksctl create cluster --name=$1 --nodes=2 --region=$2 --write-kubeconfig=false
+    eksctl create cluster --name=$1 --nodes=2 --region=$2 --zones $2a,$2b --write-kubeconfig=false
 fi
